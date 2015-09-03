2ab4e75474769123ea15cf46b46c71bfdec1fc17
apradhn <abhijit.pradhan6@gmail.com>
2015-09-03 13:03:36 -0400

    implements until loop

--- largest_decent_number.rb
+++ largest_decent_number.rb
@@ -1,19 +1,20 @@
 require "pry"
 
 def largest_decent_number(digits)
-    if digits.to_i == 1
-        number = -1
-    else
-      number = "5" * digits
-      fives_count = number.split("").count{|s| s == "5"}
-      threes_count = number.split("").count{|s| s == "3"}
-      i = -1
-      until fives_count % 3 == 0 && threes_count % 5 == 0
-        number[i] = "3"
-        i -= 1
-        fives_count = number.split("").count{|s| s == "5"}
-        threes_count = number.split("").count{|s| s== "3"}        
-      end        
+  if digits == 1
+    number = -1  
+  elsif digits % 3 == 0
+    number = "5" * digits
+  elsif digits % 5 == 0
+    number = "3" * digits
+  else
+    threes_count = 5
+    fives_count = digits - 5
+    until fives_count % 3 == 0
+      threes_count += 5
+      fives_count -= 5
     end
-    puts number  
+    number = "5"*fives_count + "3"*threes_count
+  end
+  number.to_i  
 end
--- spec/largest_decent_number_spec.rb
+++ spec/largest_decent_number_spec.rb
@@ -1,6 +1,7 @@
 describe "largest_decent_number(digits)" do
+  binding.pry
   it "returns -1 when given 1" do
-    expect(largest_decent_number(1)).to eq(1)
+    expect(largest_decent_number(1)).to eq(-1)
   end
 
   it "returns 555 when given 3" do 
@@ -14,5 +15,23 @@ describe "largest_decent_number(digits)" do
   it "returns 55555533333 when given 11" do 
     expect(largest_decent_number(11)).to eq(55555533333)
   end
-  
+
+  it "returns " + "5"*18 + "33333 when given 23" do 
+    expect(largest_decent_number(23)).to eq(55555555555555555533333)
+  end
+
+  # it "returns a number with 18 5's when given 23" do 
+  #   fives_count = largest_decent_number(23).to_s.split("").count{|s| s == "5"}
+  #   expect(fives_count).to eq(18)
+  # end
+
+  # it "returns an answer with 144 5's when given 154" do
+  #   fives_count = largest_decent_number(154).to_s.split("").count{|s| s == "5"}
+  #   expect(fives_count).to eq(144)
+  # end
+
+  # it "returns an answer with 10 3's when given 154" do
+  #   threes_count = largest_decent_number(154).to_s.split("").count{|s| s == "3"}
+  #   expect(threes_count).to eq(10)
+  # end  
 end
